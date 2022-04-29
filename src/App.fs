b1d18d14c0617b878a5f8f839ec0b83bdf9a66da
Dejan Milicic <dejan.milicic@gmail.com>
2022-04-29 16:49:19 +0200

    parsing json

--- src/App.fs
+++ src/App.fs
@@ -4,48 +4,81 @@ open Elmish
 open Elmish.React
 open Fable.SimpleHttp
 open Feliz
+open Thoth.Json
+
+type Product = { name: string; price: float }
+
+type StoreInfo =
+    { name: string
+      since: int
+      daysOpen: string list
+      products: Product list }
 
 type State =
-  { StoreInfo: Deferred<Result<string, string>> }
+    { StoreInfo: Deferred<Result<StoreInfo, string>> }
 
-type Msg =
-  | LoadStoreInfo of AsyncOperationEvent<Result<string, string>>
+type Msg = LoadStoreInfo of AsyncOperationEvent<Result<string, string>>
 
-let init() =
-  { StoreInfo = HasNotStartedYet }, Cmd.ofMsg (LoadStoreInfo Started)
+let init () =
+    { StoreInfo = HasNotStartedYet }, Cmd.ofMsg (LoadStoreInfo Started)
+
+let parseStoreInfo (json: string) : Result<StoreInfo, string> =
+  Decode.Auto.fromString<StoreInfo>(json)
 
 let update (msg: Msg) (state: State) =
-  match msg with
-  | LoadStoreInfo Started ->
-      let nextState = { state with StoreInfo = InProgress }
-      let loadStoreInfo =
-        async {
-          // simulate delay
-          do! Async.Sleep 1500
-          let! (statusCode, storeInfo) = Http.get "/store.json"
-          if statusCode = 200
-          then return LoadStoreInfo (Finished (Ok storeInfo))
-          else return LoadStoreInfo (Finished (Error "Could not load the store information"))
-        }
+    match msg with
+    | LoadStoreInfo Started ->
+        let nextState = { state with StoreInfo = InProgress }
 
-      nextState, Cmd.fromAsync loadStoreInfo
+        let loadStoreInfo =
+            async {
+                // simulate delay
+                do! Async.Sleep 1500
+                let! (statusCode, storeInfo) = Http.get "/store.json"
 
-  | LoadStoreInfo (Finished result) ->
-      let nextState = { state with StoreInfo = Resolved result }
-      nextState, Cmd.none
+                if statusCode = 200 then
+                    return LoadStoreInfo(Finished(Ok storeInfo))
+                else
+                    return LoadStoreInfo(Finished(Error "Could not load the store information"))
+            }
+
+        nextState, Cmd.fromAsync loadStoreInfo
+
+    | LoadStoreInfo (Finished (Ok storeInfoJson)) ->
+        // Here, we are able to retrieve the JSON from the server
+        // Now we try to parse thr JSON to a `StoreInfo` instance
+        match parseStoreInfo storeInfoJson with
+        | Ok storeInfo ->
+            // JSON was parsed successfully into `StoreInfo`
+            let nextState =
+                { state with StoreInfo = Resolved(Ok storeInfo) }
+
+            nextState, Cmd.none
+
+        | Error error ->
+            // JSON parsing failed here :/
+            let nextState =
+                { state with StoreInfo = Resolved(Error error) }
+
+            nextState, Cmd.none
+
+    | LoadStoreInfo (Finished (Error httpError)) ->
+        let nextState =
+            { state with StoreInfo = Resolved(Error httpError) }
+
+        nextState, Cmd.none
 
 let render (state: State) (dispatch: Msg -> unit) =
-  match state.StoreInfo with
-  | HasNotStartedYet -> Html.none
-  | InProgress -> Html.h1 "Loading..."
-  | Resolved (Error errorMsg) ->
-      Html.h1 [
-        prop.style [ style.color.red ]
-        prop.text errorMsg
-      ]
+    match state.StoreInfo with
+    | HasNotStartedYet -> Html.none
+    | InProgress -> Html.h1 "Loading..."
+    | Resolved (Error errorMsg) ->
+        Html.h1 [ prop.style [ style.color.red ]
+                  prop.text errorMsg ]
 
-  | Resolved (Ok products) ->
-      Html.pre products
+    | Resolved (Ok storeInfo) ->
+        Html.div [ Html.h1 storeInfo.name
+                   Html.ul [ for product in storeInfo.products -> Html.li product.name ] ]
 
 Program.mkProgram init update render
 |> Program.withReactSynchronous "elmish-app"
